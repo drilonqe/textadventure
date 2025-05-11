eaf039e7787e926e43438e02cff8f3bc9e312925
Drilon Qerimi <drilonqerimi@Drilons-MacBook-Air.local>
2025-05-11 15:30:30 -0500

    Created and implemented room interface

--- Room.java
+++ Room.java
@@ -1,6 +1,6 @@
 public interface Room {
     void roomEntry(); // this command describes a room after you enter
-    void wait(); // wait for a turn
+    void waitTurn(); // wait for a turn
     void go(); // player moves in a direction
     void speak(); // player speaks to an object / person
     void pickup(); // player picks up something
