7c936f1e7920f7773791a38e5a6d51646e89d3eb
Drilon Qerimi <drilonqerimi@Drilons-MacBook-Air.local>
2025-05-11 23:53:43 -0500

    Finished

--- Room.java
+++ Room.java
@@ -1,10 +1,10 @@
 public interface Room {
    public void roomEntry(); // this command describes a room after you enter
    public void waitTurn(); // wait for a turn
-   public void go(); // player moves in a direction
+   public Room go(String direction); // player moves in a direction
    public void speak(); // player speaks to an object / person
-   public void pickup(); // player picks up something
-   public void use();  // use object
-   public void attack(); // attack something
-   public void lookat(); // look or watch something
+   void pickup(String item); // player picks up something
+   void use(String item);  // use object
+   void attack(String target); // attack something
+   public void lookaround(); // look around
 }
